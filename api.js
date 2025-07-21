577790b7e2cfe37c515db2ccb375719a0a22c149
Gorav Gumber <163832113+Gauravg2630@users.noreply.github.com>
2025-07-21 10:54:55 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ' https://a5371e7a24d4.ngrok-free.app/api',
+  baseURL: 'https://a5371e7a24d4.ngrok-free.app/api',
 });
 
 export default api;
