8c9f0bc68f40f232445c3823b8c57a6daddfc0d0
Gorav Gumber <163832113+Gauravg2630@users.noreply.github.com>
2025-07-21 10:21:39 +0530

    Update contactRoutes.js

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {
-  getContacts,
-  createContact,
-  deleteContact
-} = require('../Controllers/contactController');
+const { getContacts, createContact, updateContact, deleteContact } = require('../controllers/contactController');
 
 router.get('/', getContacts);
 router.post('/', createContact);
