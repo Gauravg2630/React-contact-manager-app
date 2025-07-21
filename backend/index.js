3ad03e6e6d73c46b2282b006b02c0c92365ea0f6
Gorav Gumber <163832113+Gauravg2630@users.noreply.github.com>
2025-07-21 11:21:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 
 app.use('/api/contacts', contactRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
